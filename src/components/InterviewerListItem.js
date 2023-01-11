fb272cfaf85356406955b6d6982fb9cf10e8ff82
Brandon <bbeam1@gmail.com>
2023-01-10 19:13:02 -0700

    lists a single interviewer

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,20 @@
 import React from "react"
+import classNames from "classnames";
 import "components/InterviewerListItem.scss"
 
 export default function InterviewerListItem(props) {
+
+  let interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
   return (
-    <li className="interviewers__item">
+    <li onClick={() => props.setInterviewer(props.id)} className={interviewerClass}>
       <img
         className="interviewers__item-image"
         src="https://i.imgur.com/LpaY82x.png"
         alt="Sylvia Palmer"
       />
-      Sylvia Palmer
+      {props.selected && 'Sylvia Palmer'}
     </li>
   )
 }
