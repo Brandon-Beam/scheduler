392bc533f4142a956642e4e2c6e48dbb393d9df4
Brandon <bbeam1@gmail.com>
2023-01-11 18:52:05 -0700

    put some pieces together

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,20 @@
 import "components/Appointment/styles.scss"
-import React from "react"
+import React, { Fragment } from "react"
+import Header from "./Header"
+import Show from "./Show"
+import Empty from "./Empty"
+
+
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment"><Header time={props.time} />
+      <Fragment>
+        {props.interview ?
+          <Show student={props.interview.student} name={props.interview.interviewer.name} />
+          :
+          <Empty />}
+      </Fragment>
+    </article>
   )
 }
